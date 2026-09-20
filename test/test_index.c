92dd43f57ec0eafd89f2823df30d8345626a532e
Nartallax <nartallax@gmail.com>
2026-09-20 08:34:15 +0200

    Uncommit some tests

--- test/test_index.c
+++ test/test_index.c
@@ -52,9 +52,9 @@ int main() {
       MAKE_TEST(test_json_tokenizer_numbers),
       MAKE_TEST(test_json_tokenizer_constants),
       MAKE_TEST(test_json_tokenizer_array),
-      // MAKE_TEST(test_json_tokenizer_object),
-      // MAKE_TEST(test_json_tokenizer_whitespaces),
-      // MAKE_TEST(test_json_tokenizer_allocation_failures),
+      MAKE_TEST(test_json_tokenizer_object),
+      MAKE_TEST(test_json_tokenizer_whitespaces),
+      MAKE_TEST(test_json_tokenizer_allocation_failures),
       // tests go here
   };
 
